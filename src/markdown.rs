5cdb2777c1d71ba4a5c3f8ea6b7b52a186e60f43
immanelg <119798691+immanelg@users.noreply.github.com>
2025-03-07 13:16:30 +0300

    css already includes octicon link svg

--- src/markdown.rs
+++ src/markdown.rs
@@ -38,9 +38,7 @@ pub fn to_html(md: &str) -> String {
         Event::Text(text) => {
             if inside_heading_level {
                 let anchor = to_tag_anchor(&text);
-                Event::Html(CowStr::from(format!(r##"<a id="{anchor}" class="anchor" href="#{anchor}">
-<svg class="octicon octicon-link" viewBox="0 0 16 16" version="1.1" width="16" height="16" aria-hidden="true"><path d="m7.775 3.275 1.25-1.25a3.5 3.5 0 1 1 4.95 4.95l-2.5 2.5a3.5 3.5 0 0 1-4.95 0 .751.751 0 0 1 .018-1.042.751.751 0 0 1 1.042-.018 1.998 1.998 0 0 0 2.83 0l2.5-2.5a2.002 2.002 0 0 0-2.83-2.83l-1.25 1.25a.751.751 0 0 1-1.042-.018.751.751 0 0 1-.018-1.042Zm-4.69 9.64a1.998 1.998 0 0 0 2.83 0l1.25-1.25a.751.751 0 0 1 1.042.018.751.751 0 0 1 .018 1.042l-1.25 1.25a3.5 3.5 0 1 1-4.95-4.95l2.5-2.5a3.5 3.5 0 0 1 4.95 0 .751.751 0 0 1-.018 1.042.751.751 0 0 1-1.042.018 1.998 1.998 0 0 0-2.83 0l-2.5 2.5a1.998 1.998 0 0 0 0 2.83Z"></path></svg>
-</a>{text}"##)))
+                Event::Html(CowStr::from(format!(r##"<a id="{anchor}" class="anchor" href="#{anchor}"><span class="octicon octicon-link"></span></a>{text}"##)))
             } else {
                 Event::Text(text)
             }
