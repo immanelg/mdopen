52b2b24ad8a9d77fa0f165c9d4d98481fedda04b
immanelg <119798691+immanelg@users.noreply.github.com>
2025-03-09 22:07:49 +0300

    fix fs watcher being dropped and set it up only if enabled

--- src/watch.rs
+++ src/watch.rs
@@ -6,36 +6,32 @@ use std::sync::{Arc, RwLock};
 
 pub(crate) type WatcherBus = Arc<RwLock<bus::Bus<notify::Event>>>;
 
-pub(crate) fn setup_watcher(config: &AppConfig) -> WatcherBus {
+pub(crate) fn setup_watcher(_config: &AppConfig) -> (WatcherBus, impl Watcher) {
     let watcher_bus = Arc::new(RwLock::new(bus::Bus::new(8)));
 
     let watcher_bus_notify = watcher_bus.clone();
-
-    if config.enable_reload {
-        let mut watcher = RecommendedWatcher::new(
-            move |event: Result<notify::Event, notify::Error>| {
-                if let Ok(event) = event {
-                    use notify::EventKind as Kind;
-                    match event.kind {
-                        Kind::Remove(_) | Kind::Create(_) | Kind::Modify(_) => {
-                            debug!("watcher broadcast: {:?} {:?}", event.kind, &event.paths);
-                            let mut watcher_bus = watcher_bus_notify.write().unwrap();
-                            watcher_bus.broadcast(event);
-                        }
-                        Kind::Access(_) | Kind::Other | Kind::Any => {}
+    let mut watcher = RecommendedWatcher::new(
+        move |event: Result<notify::Event, notify::Error>| {
+            if let Ok(event) = event {
+                use notify::EventKind as Kind;
+                match event.kind {
+                    Kind::Remove(_) | Kind::Create(_) | Kind::Modify(_) => {
+                        debug!("watcher broadcast: {:?} {:?}", event.kind, &event.paths);
+                        let mut watcher_bus = watcher_bus_notify.write().unwrap();
+                        watcher_bus.broadcast(event);
                     }
+                    Kind::Access(_) | Kind::Other | Kind::Any => {}
                 }
-            },
-            notify::Config::default(),
-        )
+            }
+        },
+        notify::Config::default(),
+    )
+    .unwrap();
+    watcher
+        .watch(".".as_ref(), notify::RecursiveMode::Recursive)
         .unwrap();
-        watcher
-            .watch(".".as_ref(), notify::RecursiveMode::Recursive)
-            .unwrap();
-        debug!("watching directory: .");
-        // NOTE: https://github.com/notify-rs/notify/issues/247
-    }
-    //FIXME: return watcher so it's not drop()ed LOL
+    debug!("watching directory: .");
+    // NOTE: https://github.com/notify-rs/notify/issues/247
 
-    watcher_bus
+    (watcher_bus, watcher)
 }
