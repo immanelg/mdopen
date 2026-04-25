8aafffd78724db48c1a6c7fbeefb8557e55ba049
Alexander Gryaznov <hi@agryaznov.com>
2026-04-25 13:45:04 +0300

    add themes

--- src/app_config.rs
+++ src/app_config.rs
@@ -1,9 +1,12 @@
 #![allow(unused)]
 use std::net::SocketAddr;
 
+use crate::cli::Theme;
+
 pub(crate) struct AppConfig {
     pub addr: SocketAddr,
     pub enable_reload: bool,
     pub enable_latex: bool,
     pub enable_syntax_highlight: bool,
+    pub theme: Theme,
 }
